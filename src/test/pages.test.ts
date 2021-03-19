5f794fb07e65b1eb484b1f6868cfd3a4ecf5268d
Karol Laskowski <karol@laskowski.dev>
2021-03-19 19:46:33 +0100

    Pages test converted to TS and improved

--- src/test/pages.test.ts
+++ src/test/pages.test.ts
@@ -5,24 +5,26 @@ test('finishAndStartPageVisits stops all started visits when given domain and ti
   // arrange
   const domain1 = 'example.com';
   const domain2 = 'test.com';
-  const pages = {};
+  const pages: any = {};
   const visitStartedTime1 = new Date();
   pages[domain1] = new Page();
   pages[domain1].visits.push(new PageVisit(visitStartedTime1, null, null, 0));
+  const expected = visitStartedTime1.getTime();
 
   //act
   PageHelper.finishAndStartPageVisits(pages, domain2);
 
   //assert
   expect(pages[domain1].visits[0].to).not.toBeFalsy();
-  expect(pages[domain1].visits[0].to).toEqual(visitStartedTime1);
+  expect(pages[domain1].visits[0].to.getTime()).toBeGreaterThan(expected - 5);
+  expect(pages[domain1].visits[0].to.getTime()).toBeLessThan(expected + 5);
 });
 
 test('finishAndStartPageVisits adds new Page and PageVisit when given domain and time', () => {
   // arrange
   const domain1 = 'example.com';
   const domain2 = 'test.com';
-  const pages = {};
+  const pages: any = {};
   const visitStartedTime1 = new Date();
   pages[domain1] = new Page();
   pages[domain1].visits.push(new PageVisit(visitStartedTime1, null, null, 0));
@@ -41,7 +43,7 @@ test('finishAndStartPageVisits adds new Page and PageVisit when given domain and
 test('getTotalSpentTime return sum of visits time when added 2 visits to Page entry', () => {
   // arrange
   const domain = 'example.com';
-  const pages = {};
+  const pages: any = {};
   const now = new Date();
   const timespan3 = 9000;
   const visitStartTime3 = new Date(now.getTime() - timespan3);
@@ -52,7 +54,7 @@ test('getTotalSpentTime return sum of visits time when added 2 visits to Page en
   const breakTime1 = 7000;
   const visitEndTime1 = new Date(visitStartTime2.getTime() - breakTime1);
   const timespan1 = 6000;
-  const visitStartTime1 = new Date(visitEndTime1 - timespan1);
+  const visitStartTime1 = new Date(visitEndTime1.getTime() - timespan1);
 
   PageHelper.startPageVisits(pages, domain, visitStartTime1);
   PageHelper.finishPageVisits(pages, visitEndTime1);
