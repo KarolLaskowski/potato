26b0eb148985454df2c2f9a86f6b65526da6c29b
Karol Laskowski <karol@laskowski.dev>
2021-03-22 11:53:48 +0100

    Created tabs in options screen

--- src/js/options.ts
+++ src/js/options.ts
@@ -3,6 +3,10 @@ import Helpers from './helpers';
 import { Consts, TimeStamp } from './common';
 import { PageHelper, PageVisit } from './pages';
 import Config from './config';
+import '../scss/options.scss';
+
+let $tabButtons: Array<HTMLElement>;
+let $tabs: Array<HTMLElement>;
 
 async function render() {
   const $pagesTable = document.querySelectorAll('#pages').item(0);
@@ -10,20 +14,17 @@ async function render() {
   Config.DOM.fillTableWithData($pagesTable, data);
 }
 
-function selectButton($clickedBtn: HTMLElement) {
-  const $tabButtons: Array<HTMLElement> = Array.from(
-    document.getElementsByClassName('tab-btn') as HTMLCollectionOf<HTMLElement>
-  );
+function selectButton(
+  $clickedBtn: HTMLElement,
+  $tabButtons: Array<HTMLElement>
+) {
   for (let i = 0; i < $tabButtons.length; i++) {
-    $tabButtons[i].classList.remove('tab-btn-selected');
+    $tabButtons[i].classList.remove('is-active');
   }
-  $clickedBtn.classList.add('tab-btn-selected');
+  $clickedBtn.classList.add('is-active');
 }
 
-function selectTab(tabId: string) {
-  const $tabs: Array<HTMLElement> = Array.from(
-    document.getElementsByClassName('tab') as HTMLCollectionOf<HTMLElement>
-  );
+function selectTab(tabId: string, $tabs: Array<HTMLElement>) {
   for (let i = 0; i < $tabs.length; i++) {
     if ($tabs[i].id === tabId) {
       $tabs[i].classList.add('tab-visible');
@@ -34,18 +35,18 @@ function selectTab(tabId: string) {
 }
 
 function openTab(e: MouseEvent) {
-  const $clickedBtn: HTMLElement = e.target as HTMLElement;
-  selectButton($clickedBtn);
+  e.preventDefault();
+  const $clickedBtn: HTMLElement = this as HTMLElement;
+  selectButton($clickedBtn, $tabButtons);
   const tabId: string = $clickedBtn.getAttribute('data-for');
-  selectTab(tabId);
+  selectTab(tabId, $tabs);
 }
 
 function initTabButtons() {
-  const tabButtons: Array<HTMLElement> = Array.from(
-    document.getElementsByClassName('tab-btn') as HTMLCollectionOf<HTMLElement>
-  );
-  for (let i = 0; i < tabButtons.length; i++) {
-    tabButtons[i].addEventListener('click', openTab);
+  $tabButtons = Array.from(document.querySelectorAll('div.tabs > ul > li'));
+  $tabs = Array.from(document.querySelectorAll('div.tab'));
+  for (let i = 0; i < $tabButtons.length; i++) {
+    $tabButtons[i].addEventListener('click', openTab);
   }
 }
 
